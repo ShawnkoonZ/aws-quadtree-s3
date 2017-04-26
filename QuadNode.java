fdeeeaf4b64495a04e9de717dbe9f9de0c8c01cb
cwade12c <cwade12c@gmail.com>
2017-04-26 10:17:03 -0700

    Added a new class to represent a Node in the QuadTree.
    The QuadNode contains properties for the x and y coordinates, the data, and four inner quad nodes (to represent each region of the graph).
    The QuadNode contains behaviors for getting x/y coordinates, getting the data, getting the inner quad nodes, setting x/y coordinates, setting the data, and setting the inner quad nodes.

---
+++ QuadNode.java
@@ -0,0 +1,77 @@
+public class QuadNode{
+   private double x, y;
+   private QuadNode NE, NW, SW, SE;
+   private double data;
+   
+   public QuadNode(double xPos, double yPos, double value){
+      this.x = xPos;
+      this.y = yPos;
+      this.data = value;
+   }
+   
+   public double getXCoordinate(){
+      return this.x;
+   }
+   
+   public double getYCoordinate(){
+      return this.y;
+   }
+   
+   public double[] getCoordinates(){
+      double[] coordinateArray = {this.x, this.y};
+      return coordinateArray;
+   }
+   
+   public double getData(){
+      return this.data;
+   }
+   
+   public QuadNode getQuadNodeNE(){
+      return this.NE;
+   }
+   
+   public QuadNode getQuadNodeNW(){
+      return this.NW;
+   }
+   
+   public QuadNode getQuadNodeSW(){
+      return this.SW;
+   }
+   
+   public QuadNode getQuadNodeSE(){
+      return this.SE;
+   }
+   
+   public QuadNode[] getQuadNodes(){
+      QuadNode[] nodeArray = {this.NE, this.NW, this.SW, this.SE};
+      return nodeArray;
+   }
+   
+   public void setXCoordinate(double xPos){
+      this.x = xPos;
+   }
+   
+   public void setYCoordinate(double yPos){
+      this.y = yPos;
+   }
+   
+   public void setData(double value){
+      this.data = value;
+   }
+   
+   public void setQuadNodeNE(QuadNode NE){
+      this.NE = NE;
+   }
+   
+   public void setQuadNodeNW(QuadNode NW){
+      this.NW = NW;
+   }
+   
+   public void setQuadNodeSW(QuadNode SW){
+      this.SW = SW;
+   }
+   
+   public void setQuadNodeSE(QuadNode SE){
+      this.SE = SE;
+   }
+}
