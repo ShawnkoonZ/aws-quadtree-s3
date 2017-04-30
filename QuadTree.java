98600519c18c8ff37321103233b7c8e7ba3093e0
cwade12c <cwade12c@gmail.com>
2017-04-29 19:27:21 -0700

    Updated the QuadTree to be able to generate output given a grid
    Added a generateQuadTree method that will generate output based on the scale of the QuadTree

--- QuadTree.java
+++ QuadTree.java
@@ -3,59 +3,51 @@
  * Composed with QuadTree Datastructure.
  */
 public class QuadTree {
-  private double xMin, xMax, yMin, yMax;
-  private QuadNode root;
+   private double xMin, xMax, yMin, yMax;
+   private int index;
   
-  public QuadTree(){
-   this.xMin = -1000;
-   this.xMax = 1000;
-   this.yMin = -1000;
-   this.yMax = 1000;
-  }
+   public QuadTree(){
+      this.xMin = -1000;
+      this.xMax = 1000;
+      this.yMin = -1000;
+      this.yMax = 1000;
+      this.index = 0;
+   }
 
-  public QuadTree(double xLow, double xHigh, double yLow, double yHigh) {
-    this.xMin = xLow;
-    this.xMax = xHigh;
-    this.yMin = yLow;
-    this.yMax = yHigh;
-  }
-  
-  public void insert(double xCoordinate, double yCoordinate, double value){
-    boolean isWithinBounds = this.checkBounds(xCoordinate, yCoordinate);
-    
-    if(!isWithinBounds){
-      System.out.println("The coordinates [" + xCoordinate + "," + yCoordinate + "] are out of bounds! Skipping insertion...");
-      return;
-    }
-    
-    this.root = this.insert(this.root, xCoordinate, yCoordinate, value);
-  }
-  
-  private QuadNode insert(QuadNode node, double xCoordinate, double yCoordinate, double value){  
-    if(node == null){
-      return new QuadNode(xCoordinate, yCoordinate, value);
-    }
-    
-    if((xCoordinate > node.getXCoordinate()) && (yCoordinate > node.getYCoordinate())){
-      node.setQuadNodeNE(insert(node.getQuadNodeNE(), xCoordinate, yCoordinate, value));
-    }
-    
-    if((xCoordinate < node.getXCoordinate()) && (yCoordinate > node.getYCoordinate())){
-      node.setQuadNodeNW(insert(node.getQuadNodeNW(), xCoordinate, yCoordinate, value));
-    }
-    
-    if((xCoordinate < node.getXCoordinate()) && (yCoordinate < node.getYCoordinate())){
-      node.setQuadNodeSW(insert(node.getQuadNodeSW(), xCoordinate, yCoordinate, value));
-    }
-    
-    if((xCoordinate > node.getXCoordinate()) && (yCoordinate < node.getYCoordinate())){
-      node.setQuadNodeSE(insert(node.getQuadNodeSE(), xCoordinate, yCoordinate, value));
-    }
-    
-    return node;
-  }
-  
-  private boolean checkBounds(double xCoordinate, double yCoordinate){
-    return xCoordinate >= this.xMin && xCoordinate <= this.xMax && yCoordinate >= this.yMin && yCoordinate <= this.yMax;
-  }
+   public QuadTree(double xLow, double xHigh, double yLow, double yHigh){
+      this.xMin = xLow;
+      this.xMax = xHigh;
+      this.yMin = yLow;
+      this.yMax = yHigh;
+      this.index = 0;
+   }
+   
+   public void generateQuadTree(){
+      System.out.println("Index: <Node Number>, [(<xMin>,<xMax>),(<yMin>,<yMax>)]");
+      this.generateQuadTree(this.xMin, this.xMax, this.yMin, this.yMax);
+   }
+   
+   private boolean isWithinGap(int gap){
+      return (this.xMax - this.xMin == gap) || (this.yMax - this.yMin == gap);
+   }
+   
+   private void generateQuadTree(double xMin, double yMin, double xMax, double yMax){                         
+      if(this.isWithinGap(1)){return;}
+
+      System.out.println("Index: " + this.index++ + ", [(" + xMin + 
+                         "," + yMin + "),(" + xMax + 
+                         "," + yMax + ")]");      
+      
+      this.generateQuadTree(xMin, (yMax - yMin) / 2,
+                            (xMax - xMin) / 2, yMax);
+                            
+      this.generateQuadTree(xMin, yMin, (xMax - xMin) / 2,
+                            (yMax - yMin) / 2);
+                            
+      this.generateQuadTree((xMax - xMin) / 2, yMin, xMax,
+                            (yMax - yMin) / 2);
+                            
+      this.generateQuadTree((xMin - yMin) / 2, (yMin - yMax) / 2,
+                            yMin, yMax);
+   }
 }
--- QuadTreeTester.java
+++ QuadTreeTester.java
@@ -3,10 +3,8 @@
  * Composed with User logics.
  */
 public class QuadTreeTester {
-  public static void main(String[] args) {
-    QuadTree tree = new QuadTree();
-    tree.insert(100,100,5);
-    tree.insert(99,200,3);
-    tree.insert(-4949,39038,45);//The coordinates [-4949.0,39038.0] are out of bounds! Skipping insertion...
-  }
+   public static void main(String[] args) {
+      QuadTree tree = new QuadTree(0,0,8,8);
+      tree.generateQuadTree();  
+   }
 }
