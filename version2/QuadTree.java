2cf1b5276fb1fc7c8e1eb61f3812e14d0146d240
shawnkoon <shawn9405@gmail.com>
2017-05-06 10:31:16 -0700

    Refacotred indentation + var name + stubbed generate method added

--- version2/QuadTree.java
+++ version2/QuadTree.java
@@ -2,6 +2,7 @@ package version2;
 
 public class QuadTree {
   private double xLow, yLow, xHigh, yHigh, minimumGap;
+  private int partitionLimit;
 
   private init(double xLow, double yLow, double xHigh, double yHigh, double minimumGap) {
     this.xLow = xLow;
@@ -23,27 +24,31 @@ public class QuadTree {
 	private boolean isPowerTwo(double number) { return (number & (number - 1)) == 0; }
 
   private void checkForExit() {
-		if (this.xHigh > 0 && this.yHigh > 0) {
-			if(this.xHigh == this.yHigh) {
-				if (this.isPowerTwo(this.xHigh)) {
-					if(!this.isPowerTwo(this.yHigh)) {
-						System.out.println("Error : High-Y needs to be power of 2.");
-						System.exit(-1);
-					}
-				}
-				else {
-					System.out.println("Error : High-X needs to be power of 2.");
-					System.exit(-1);
-				}							
-			}
-			else {
-				System.out.println("Error : High-X & High-Y needs to be equal.");
-				System.exit(-1);
-			}
-		}
-		else {
-			System.out.println("Error : High-X and High-Y needs to be positive Number.");
-			System.exit(-1);
-		}
-	}
+    if (this.xHigh > 0 && this.yHigh > 0) {
+      if(this.xHigh == this.yHigh) {
+        if (this.isPowerTwo(this.xHigh)) {
+          if(!this.isPowerTwo(this.yHigh)) {
+            System.out.println("Error : High-Y needs to be power of 2.");
+            System.exit(-1);
+          }
+        }
+        else {
+          System.out.println("Error : High-X needs to be power of 2.");
+          System.exit(-1);
+        }							
+      }
+      else {
+        System.out.println("Error : High-X & High-Y needs to be equal.");
+        System.exit(-1);
+      }
+    }
+    else {
+      System.out.println("Error : High-X and High-Y needs to be positive Number.");
+      System.exit(-1);
+    }
+  }
+
+  public void generateQuadTree(int partitionLimit) {
+    
+  }
 }
