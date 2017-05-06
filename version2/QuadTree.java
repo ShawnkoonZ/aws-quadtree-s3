26258f009f14529f55387e37b108f732fb65ee15
shawnkoon <shawn9405@gmail.com>
2017-05-06 12:37:00 -0700

    Generates tree in correct Quadrant order + refactoring code

--- version2/QuadTree.java
+++ version2/QuadTree.java
@@ -1,15 +1,22 @@
+/*
+*  -------------
+* |  Q2  |  Q1  |
+* |-------------|
+* |  Q3  |  Q4  |
+*  -------------
+*/
 package version2;
 
 import java.util.*;
 
 public class QuadTree {
-  private TreeNode curNode;
+  private TreeNode rootNode;
   private double minimumGap;
   private long partitionLimit, nodeNumber;
 
   private void init(double xLow, double yLow, double xHigh, double yHigh, double minimumGap) {
     this.nodeNumber = 0;
-    curNode = new TreeNode(xLow, yLow, xHigh, yHigh);
+    this.rootNode = new TreeNode(xLow, yLow, xHigh, yHigh);
     this.minimumGap = minimumGap;
     this.checkForExit();
   }
@@ -25,10 +32,10 @@ public class QuadTree {
 	private boolean isPowerTwo(int number) { return (number & (number - 1)) == 0; }
 
   private void checkForExit() {
-    if (this.curNode.xHigh > 0 && this.curNode.yHigh > 0) {
-      if(this.curNode.xHigh == this.curNode.yHigh) {
-        if (this.isPowerTwo((int)this.curNode.xHigh)) {
-          if(!this.isPowerTwo((int)this.curNode.yHigh)) {
+    if (this.rootNode.xHigh > 0 && this.rootNode.yHigh > 0) {
+      if(this.rootNode.xHigh == this.rootNode.yHigh) {
+        if (this.isPowerTwo((int)this.rootNode.xHigh)) {
+          if(!this.isPowerTwo((int)this.rootNode.yHigh)) {
             System.out.println("Error : High-Y needs to be power of 2.");
             System.exit(-1);
           }
@@ -50,19 +57,39 @@ public class QuadTree {
   }
 
   public void generateQuadTree(long partitionLimit) {
-    System.out.printf("Node : %d, (%f, %f, %f, %f)\n", this.nodeNumber, this.curNode.xLow, this.curNode.yLow, this.curNode.xHigh, this.curNode.yHigh);
-    Queue<TreeNode> hi = new LinkedList<TreeNode>();
+    Queue<TreeNode> treeQueue = new LinkedList<TreeNode>();
+    treeQueue.add(this.rootNode);
+    TreeNode curNode = treeQueue.poll();
 
-    
+    while(curNode != null) {
+      System.out.printf("Node : %d | (%f, %f, %f, %f)\n", this.nodeNumber++, curNode.xLow, curNode.yLow, curNode.xHigh, curNode.yHigh);
+
+      if (!curNode.isLeafNode()) {
+        // Q1
+        treeQueue.add(new TreeNode((curNode.xHigh - curNode.xLow)/2 + curNode.xLow, (curNode.yHigh - curNode.yLow)/2 + curNode.yLow, curNode.xHigh, curNode.yHigh));
+        // Q2
+        treeQueue.add(new TreeNode(curNode.xLow, (curNode.yHigh - curNode.yLow)/2 + curNode.yLow, (curNode.xHigh - curNode.xLow)/2  + curNode.xLow, curNode.yHigh));
+        // Q3
+        treeQueue.add(new TreeNode(curNode.xLow, curNode.yLow, (curNode.xHigh - curNode.xLow)/2 + curNode.xLow, (curNode.yHigh - curNode.yLow)/2 + curNode.yLow));
+        // Q4
+        treeQueue.add(new TreeNode((curNode.xHigh - curNode.xLow)/2 + curNode.xLow, curNode.yLow, curNode.xHigh, (curNode.yHigh - curNode.yLow)/2 + curNode.yLow));
+      }
+      curNode = treeQueue.poll();
+    }
+    System.out.println("# Total Elements : " + this.nodeNumber);
   }
 
   private class TreeNode {
     private double xLow, yLow, xHigh, yHigh;
-    public TreeNode(double xLow, double yLow, double xHigh, double yHigh) {
+    private TreeNode(double xLow, double yLow, double xHigh, double yHigh) {
       this.xLow = xLow;
       this.yLow = yLow;
       this.xHigh = xHigh;
       this.yHigh = yHigh;
+    }
+
+    private boolean isLeafNode() {
+      return (this.xHigh - this.xLow) == 1 || (this.yHigh - this.yLow) == 1;
     }
   }
 }
