8f449ff1dd10b8b58565a16b5bb95c8477b74b73
cwade12c <cwade12c@gmail.com>
2017-05-06 22:07:44 -0700

    Added an install file
    Added a shell script to help the user install the Amazon SDK that the java files require.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin
+
+wget https://sdk-for-java.amazonwebservices.com/latest/aws-java-sdk.zip
+
+unzip -q aws-java-sdk.zip
+
+rm aws-java-sdk.zip
+
+mv $(ls|grep aws-java-sdk-*) vendor
+
+rm vendor/*.txt && rm vendor/*.html && rm -rf vendor/samples && rm -rf vendor/documentation
+
+mkdir vendor/amazon && mkdir vendor/amazon/aws-java-sdk
+
+mv vendor/lib vendor/amazon/aws-java-sdk && mv vendor/third-party vendor/amazon/aws-java-sdk
